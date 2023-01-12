07e6cb5a14522e5dc918107b76f7215853cbb9ec
Andrey Lisovsky <mitolcore1234@gmail.com>
2023-01-12 07:18:43 +0300

    feature: create new section homes

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Homes } from '../Homes';
+import { Sprite } from '../Sprite';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Sprite />
+      <Homes />
+    </>
+  );
+}
+
+export default App;
